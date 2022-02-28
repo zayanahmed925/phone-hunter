92ad5fd68d839f8a5efc7a19bfbcad9ba311168c
zayanahmed925 <rjraju925@gmail.com>
2022-02-28 23:40:47 +0600

    get all brand

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 const loadPhone = () => {
     const searchField = document.getElementById('scarch-field');
     const searchFieldText = searchField.value;
-    console.log(searchFieldText);
+    const ul = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
+    fetch(ul)
+        .then(res => res.json())
+        .then(data => searchResult(data))
+    // console.log(searchFieldText);
+}
+
+const searchResult = (phones) => {
+    // console.log(phones.data)
+    phones.data.forEach(phone => {
+        console.log(phone)
+    });
+
 }
