78288f7b27213d43e1978b9f9f582ab6ce6307b8
zayanahmed925 <rjraju925@gmail.com>
2022-03-01 00:09:56 +0600

    search result display done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const loadPhone = () => {
     const searchField = document.getElementById('scarch-field');
     const searchFieldText = searchField.value;
+    searchField.value = '';
     const ul = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`
     fetch(ul)
         .then(res => res.json())
@@ -10,8 +11,22 @@ const loadPhone = () => {
 
 const searchResult = (phones) => {
     // console.log(phones.data)
+    const displaySearchResult = document.getElementById('phones-area');
     phones.data.forEach(phone => {
         console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">Brand: ${phone.brand}</p>
+                </div>
+                <button type="button" class="btn btn-outline-info">Details</button>
+            </div>
+        `
+        displaySearchResult.appendChild(div);
     });
 
 }
